02464e5b30ac3949973e67cc2120bc0bfc12f870
JuchengHu <36852779+smgjch@users.noreply.github.com>
2024-04-20 22:08:54 +0000

    add result

---
+++ Result/DynamicVariableFolding.java
@@ -0,0 +1,47 @@
+package .workspaces.Java-Peephole-Code-Optimizer.optimised.classes.comp0012.target;
+
+public class DynamicVariableFolding {
+  public int methodOne() {
+    return 1301;
+    // int a = 42;
+    // int b = (a + 764) * 3;
+    // a = b - 67;
+    // return b + 1234 - a;
+  }
+  
+  public boolean methodTwo() {
+    System.out.println(true);
+    return true;
+    // int x = 12345;
+    // int y = 54321;
+    // System.out.println(x < y);
+    // y = 0;
+    // return x > y;
+  }
+  
+  public int methodThree() {
+    return 84;
+    // int i = 0;
+    // int j = i + 3;
+    // i = j + 4;
+    // j = i + 5;
+    // return i * j;
+  }
+  
+  public int methodFour() {
+    System.out.println(4.603228141221259E9D);
+    for (byte b = 0; b < 10; b++)
+      System.out.println(-1234 * b); 
+    return 24;
+
+    // int a = 534245;
+    // int b = a - 1234;
+    // System.out.println((120298345 - a) * 38.435792873);
+    // for(int i = 0; i < 10; i++){
+    //     System.out.println((b - a) * i);
+    // }
+    // a = 4;
+    // b = a + 2;
+    // return a * b;
+  }
+}
---
+++ Result/SimpleFolding.java
@@ -0,0 +1,8 @@
+package .workspaces.Java-Peephole-Code-Optimizer.optimised.classes.comp0012.target;
+
+public class SimpleFolding {
+  public void simple() {
+    System.out.println(12412);
+    // System.out.println(67 + 12345);
+  }
+}
